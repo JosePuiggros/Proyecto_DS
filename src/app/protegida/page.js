29f94caec9e78ac57546c4a282478e25ca1401c3
jfritzm <josefritzm@gmail.com>
2024-12-10 16:14:23 -0300

    3ra es la vencida

--- src/app/protegida/page.js
+++ src/app/protegida/page.js
@@ -1,12 +1,18 @@
 import ProtectedRoute from '../../components/ProtectedRoute';
 
-export default function ProtectedPage() {
+function page() {
   return (
     <ProtectedRoute>
       <div>
         <h1>Página Protegida</h1>
         <p>Solo puedes ver esto si estás autenticado.</p>
+        <img
+          src="ReceFitsLogo.png"
+          alt="ReceFits Logo"
+          className="w-32 sm:w-40"
+        />
       </div>
-    </ProtectedRoute>
+     </ProtectedRoute>
   );
 }
+export default page;
